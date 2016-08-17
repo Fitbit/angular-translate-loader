3850466e4c7d1d703735610fed15ee73d01aacb0
Marat Dreizin <marat.dreizin@gmail.com>
2016-08-17 11:47:39 +0300

    fix(defaultOptions): now `localeInterpolate` is environment independent

--- src/defaultOptions.js
+++ src/defaultOptions.js
@@ -1,3 +1,16 @@
+import {
+    sep
+} from 'path';
+import {
+    escapeRegExp
+} from 'lodash';
+
+/**
+ * @private
+ * @type {String}
+ */
+const SEP = escapeRegExp(sep);
+
 /**
  * @private
  */
@@ -8,8 +21,8 @@ const DEFAULT_OPTIONS = {
     localeInterpolate: [
         /_[a-z]{2}_[A-Z]{2}\./, // './test/fixtures/foo_en_US.json'
         /_[a-z]{2}\./, // './test/fixtures/foo_en.json'
-        /[\/][a-z]{2}_[A-Z]{2}[\/]/, // './test/fixtures/en_US/foo.json'
-        /[\/][a-z]{2}[\/]/ // './test/fixtures/en/foo.json'
+        new RegExp(`${SEP}[a-z]{2}_[A-Z]{2}${SEP}`), // './test/fixtures/en_US/foo.json'
+        new RegExp(`${SEP}[a-z]{2}${SEP}`) // './test/fixtures/en/foo.json'
     ],
     defaultLocale: 'en_US'
 };
