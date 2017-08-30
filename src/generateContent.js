6cb6e122a307a35b0a3c8990c58b04746a97a2ec
Ardeshir81 <ardeshireo@gmail.com>
2017-08-30 13:01:34 +0430

    fix(generate-content):remove that `must` and load angular like `ngtemplate-laoder`
    Signed-off-by: Ardeshir81 <ardeshireo@gmail.com>

--- src/generateContent.js
+++ src/generateContent.js
@@ -4,17 +4,16 @@ import DEFAULT_OPTIONS from './defaultOptions';
  * @param {String} module
  * @param {String} locale
  * @param {Object} translations
- * @param {Boolean} mustRequireAngular
+ * @param {Boolean} requireAngular
  * @return {String}
  */
-export default (module = DEFAULT_OPTIONS.module, locale = DEFAULT_OPTIONS.defaultLocale, translations = {}, mustRequireAngular = DEFAULT_OPTIONS.requireAngular) => {
-    return `${mustRequireAngular ? 'var angular = require("angular");' : ''}
-var translations = ${JSON.stringify(translations, null, '\t')};
+export default (module = DEFAULT_OPTIONS.module, locale = DEFAULT_OPTIONS.defaultLocale, translations = {}, requireAngular = DEFAULT_OPTIONS.requireAngular) => {
+    return `var translations = ${JSON.stringify(translations, null, '\t')};
 var module;
 try {
-\tmodule = angular.module("${module}");
+\tmodule = ${requireAngular ? 'var angular = require("angular");' : 'window.'}angular.module("${module}");
 } catch(err) {
-\tmodule = angular.module("${module}", ["pascalprecht.translate"]);
+\tmodule = ${requireAngular ? 'var angular = require("angular");' : 'window.'}angular.module("${module}", ["pascalprecht.translate"]);
 }
 module.config(["$translateProvider", function($translateProvider) {
 \t$translateProvider.translations("${locale}", translations);
