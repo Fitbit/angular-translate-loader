4e077ac2426b520fadcb1843748da30c992d9aed
Ardeshir81 <ardeshireo@gmail.com>
2017-08-30 13:00:35 +0430

    fix(default-options):set requireAngular default to true
    Signed-off-by: Ardeshir81 <ardeshireo@gmail.com>

--- src/defaultOptions.js
+++ src/defaultOptions.js
@@ -12,7 +12,7 @@ const DEFAULT_OPTIONS = {
         /[/\\][a-z]{2}[/\\]/ // './test/fixtures/en/foo.json'
     ],
     defaultLocale: 'en_US',
-    requireAngular: false
+    requireAngular: true
 };
 
 export default DEFAULT_OPTIONS;
